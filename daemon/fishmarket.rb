b6d90c45ccf5bacbfc77a86df6e47979169294a1
txema <txema@Lab35Srv.(none)>
2014-02-13 10:50:21 +0100

    fishmonger review

--- daemon/fishmarket.rb
+++ daemon/fishmarket.rb
@@ -3,22 +3,42 @@
 
 require 'thread'
 
-clients = Array.new(10)
-i = 0
-j = 0
+class Client
 
-for i in 0..10
-  clients.push "Cliente #{i} creado"
-  puts "Cliente #{i} creado"
-  puts "Numero de ticket: #{i}"
+  @@total_clients = 0
+
+   def initialize
+      @@total_clients += 1
+      @ticket = @@total_clients
+      puts "Cliente creado. Me han dado el ticket #{@ticket}."
+   end
+
+   def served
+      puts "Client numbered #{@ticket} is served."
+   end
+
 end
 
-fishmonger = Thread.new do
-  1.upto(10) {
-    sleep 2
-    puts "Cliente #{i} atendido"
-    puts "Proximo cliente: #{i + 1}"
-    j += 1
-  }
+
+clients = []
+mutex = Mutex.new
+number_of_clients_semaphore = ConditionVariable.new
+
+clienter = Thread.new do
+   while 1 do  
+        sleep rand(2)
+        clients.push Client.new 
+        number_of_clients_semaphore.signal
+   end
 end
-fishmonger.join unless ARGV[0]
+
+# fishmonger starts
+  while 1 do   
+    mutex.synchronize do
+      sleep rand(5)
+      number_of_clients_semaphore.wait(mutex)
+      clients.shift.served # shift quita el primer elemento y desplaza a todos los demas.
+    end
+  end
+
+clienter.join unless ARGV[0]
